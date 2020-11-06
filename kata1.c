31e59140d3b702112f289a73f415fc77e6a27111
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-06 10:23:22 +0300

    Update kata1.c

--- kata1.c
+++ kata1.c
@@ -2,3 +2,12 @@
 int multiply(int a, int b) {
   return a * b;
 }
+
+//Test
+#include <criterion/criterion.h>
+int multiply(int, int);
+
+Test(ExampleTests, should_pass_all_the_tests_provided) {
+    cr_assert_eq(multiply(5, 6), 30);
+    cr_assert_eq(multiply(12, 12), 144);
+}
